a83ee3f2215f0bd25dd2a6f016584d0d4b9810f8
MarynaIlchenko <debovamail@gmail.com>
2022-02-19 12:52:01 +0200

    html part1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './sass/main.scss';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+
+const refs = {
+  searchForm: document.querySelector('form#search-form'),
+  ,
+};
---
+++ src/js/fetch-gallery.js
@@ -0,0 +1,4 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '25774072-c5897649f594e9daa0e2ccc0a';
+
+fetch('');
