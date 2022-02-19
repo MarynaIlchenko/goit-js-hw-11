2724b7ebd3c6bd75f55674cd2121c4e4b381957e
MarynaIlchenko <debovamail@gmail.com>
2022-02-19 14:19:00 +0200

    upgrade

--- src/index.js
+++ src/index.js
@@ -6,5 +6,21 @@ import axios from 'axios';
 
 const refs = {
   searchForm: document.querySelector('form#search-form'),
-  ,
+  articlesContainer: document.querySelector('.js-articles-container'),
 };
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  const options = {
+    headers: {
+      Authorization: '25774072-c5897649f594e9daa0e2ccc0a',
+    },
+  };
+  const url = '';
+}
+
+fetch('https://pixabay.com/api/?key=cat', options)
+  .then(r => r.json())
+  .then(console.log);
